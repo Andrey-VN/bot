d7df0606ecc9b52156a71191aac19d519db35857
Andrey <andre-vylegzhani@yandex.ru>
2023-05-20 16:34:14 +0300

    --

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -27,16 +27,12 @@ func main() {
 	}
 
 	updates := bot.GetUpdatesChan(u)
-
-	// if err!=nil {
-	// 	log.Panic(err)
-	// }
-
 	productService := product.NewService()
-
 	commander := commands.NewCommander(bot, productService)
 
 	for update := range updates {
+		// commander.HandleUpdate(update)
+
 		if update.Message == nil { // If we got a message
 			continue
 		}
@@ -46,6 +42,8 @@ func main() {
 			commander.Help(update.Message)
 		case "list":
 			commander.List(update.Message)
+		case "get":
+			commander.Get(update.Message)
 		default:
 			commander.Default(update.Message)
 		}
