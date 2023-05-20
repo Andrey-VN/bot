2dc79185103726b8360c41ce3d3520f3a7db49b8
Andrey <andre-vylegzhani@yandex.ru>
2023-05-20 12:41:42 +0300

    add helpCommand

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+const token = "6230019625:AAGeXKRhPWRBNbGmkNf2hblyUm4YnN2H_Co"
 
 func main() {
-	fmt.Println("test")
+	// token := os.Getenv("TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	// if err!=nil {
+	// 	log.Panic(err)
+	// }
+
+	for update := range updates {
+		if update.Message == nil { // If we got a message
+			continue
+		}
+
+		switch update.Message.Command() {
+		case "help":
+			helpCommand(bot, update.Message)
+		default:
+			defaultBehavior(bot, update.Message)
+		}
+	}
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "/help - help")
+
+	bot.Send(msg)
+}
+
+func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Ты написал: "+inputMessage.Text)
+
+	bot.Send(msg)
 }
